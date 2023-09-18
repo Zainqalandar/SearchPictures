97e487ea64ab456e1ad6a239beb7294c92e595bb
Zainqalandar <zainqlandar@gmail.com>
2023-09-18 16:09:31 +0500

    add darkmode

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import Cards from './components/Cards'
 import Searchimag from './components/Searchimag'
+import ThemeBtn from './components/Themebtn'
 
 function App() {
   console.log('Apps')
   const [term, setTerm] = useState('life')
   const [onLoading, setOnLoading] = useState(true)
   const [images, setImages] = useState([])
+  const [darkMode, setdarkMode] = useState('light')
 
   useEffect(() => {
     fetch(`https://pixabay.com/api/?key=39461836-2e849ef976e11309a6f81194c&q=${term}&image_type=photo`)
@@ -18,16 +20,33 @@ function App() {
       })
       .catch(err => console.log(err))
   }, [term])
+  useEffect(() => {
+    document.querySelector('html').classList.remove('dark', 'light')
+    document.querySelector('html').classList.add(darkMode)
+  }, [darkMode])
+
+  let handleClick = () => {
+    if (darkMode == 'dark') {
+      setdarkMode('light')
+    }
+    else {
+      setdarkMode('dark')
+    }
+
+  }
   return (
-    <>
-      <div>
+    <div className='dark:bg-gray-800'>
+      <div className=''>
+        <div className=' relative top-4 left-3 '>
+          <ThemeBtn handleClick={handleClick} darkMode={darkMode}/>
+        </div>
         <Searchimag setTerm={setTerm} />
       </div>
       <div className=' grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-center gap-5 p-2 border-2'>
         {!onLoading && images.length == 0 && <h1 className=' text-4xl font-bold text-center border'>Image not Found</h1>}
-        <Cards  onLoading={onLoading} images={images} />
+        <Cards onLoading={onLoading} images={images} />
       </div>
-    </>
+    </div>
   )
 }
 
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 function Cards({ images, onLoading }) {
     return (
         <>
-            {onLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> : images.map(image => <div key={image.id} className="max-w-sm rounded overflow-hidden shadow-lg">
+            {onLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> : images.map(image => <div key={image.id} className=" dark:border-gray-700 max-w-sm rounded overflow-hidden shadow-lg">
                 <img className=' w-full' src={image.webformatURL} alt="" />
-                <div className="px-6 py-4">
+                <div className="dark:bg-gray-700 dark:text-white px-6 py-4">
                     <div className="font-bold text-purple-500 text-xl mb-2">
                         Photo by {image.user}
                     </div>
@@ -24,8 +24,8 @@ function Cards({ images, onLoading }) {
                         </li>
                     </ul>
                 </div>
-                <div className="px-6 pt-4 pb-2">
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#{image.tags}</span>
+                <div className="px-6 pt-4 pb-2 dark:bg-gray-700 dark:text-white">
+                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#Intertain</span>
                     <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
                     <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
                 </div>
